bcb736fa3a8b556da0f7d9177b1ea16325ee1b3b
WRakocki <wiktor.rakocki6@protonmail.com>
2024-02-24 02:05:59 +0100

    Added comments

--- die_visual.py
+++ die_visual.py
@@ -4,16 +4,20 @@ from plotly import offline
 from die import Die
 
 dice = []
+
+#User input
 dice_num = int(input("Number of dice: "))
 dice_sides = int(input("Number of dice sides: "))
 roll_num = int(input("Number of rolls: "))
 
+#Adding dice into list
 for die in range(dice_num):
     die = Die(dice_sides)
     dice.append(die)
 
-results = []
+
 #Rolling the dice and saving results
+results = []
 for roll in range(roll_num):
     result = 0
     for die in dice:
@@ -22,12 +26,14 @@ for roll in range(roll_num):
 
 
 
-#Analyzing results
-frequencies = []
 
+#Calculating maximum result of the roll
 max_result = 0
 for die in dice:
     max_result += die.num_sides
+
+#Analyzing results
+frequencies = []
 
 for value in range(1, max_result + 1):
     frequency = results.count(value)
